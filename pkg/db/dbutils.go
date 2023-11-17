fc83c66c1fe2fcc2d77b3a65c35a7d8ec33846d4
Muhammed Rajab <muhammedrajab101@gmail.com>
2023-11-17 13:13:41 +0530

    implemented the blog model to actually interact with the database. implemented db file handle database connection and manage a global mongodb client

---
+++ pkg/db/dbutils.go
@@ -0,0 +1,80 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+/*
+	--------------
+	| dbutils.go |
+	--------------
+	Utilities to connect to mongodb and keep a global variable to be accessed from anywhere in the file after calling the init and connect method once in the main function.
+*/
+
+// Global client
+var mdb *MongoDB
+
+type MongoDB struct {
+	Url            string
+	hasInitialised bool
+	Client         *mongo.Client
+}
+
+// Function to initialize the MongoDB object for global access
+func Init(url string) {
+	mdb = &MongoDB{
+		Url:            url,
+		hasInitialised: true,
+		Client:         nil,
+	}
+}
+
+// Function to return the global MongoDB object
+func GetMDB() *MongoDB {
+	mdb.panicIfNotInitialised()
+	return mdb
+}
+
+// Method to panic if the object has not been initialised
+func (m *MongoDB) panicIfNotInitialised() {
+	if !(m.hasInitialised) {
+		panic(errors.New("MongoDB object not initialised"))
+	}
+}
+
+// Method to connect to the database
+func (m *MongoDB) Connect() {
+
+	opt := options.Client()
+	opt.ApplyURI(m.Url)
+
+	c, err := mongo.Connect(context.Background(), opt)
+	if err != nil {
+		log.Fatalf("Error while connecting to mongodb: %v", err)
+	}
+
+	// Gets timeout from env in seconds
+	timeout, err := strconv.Atoi(os.Getenv("MONGODB_CONNECTION_TIMEOUT"))
+	if err != nil {
+		timeout = 10
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer cancel()
+
+	if err := c.Ping(ctx, nil); err != nil {
+		log.Fatalf("Failed to connect to mongodb: %v", err)
+		return
+	}
+
+	// Log if everything went successful (change into only on development mode)
+	log.Printf("Successfully connected to mongodb: %s\n", m.Url)
+	m.Client = c
+}
---
+++ pkg/models/blog.go
@@ -0,0 +1,228 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gosimple/slug"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type BlogModel struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" validate:"omitempty"`
+	Title     string             `bson:"title" validate:"required"`
+	Content   string             `bson:"content" validate:""`
+	Tags      []string           `bson:"tags" validate:""`
+	CreatedAt time.Time          `bson:"created_at" validate:""`
+	Slug      string             `bson:"slug" validate:""`
+	Published bool               `bson:"published" validate:""`
+}
+
+func (b *BlogModel) String() string {
+	return fmt.Sprintf(`
+Title: %s
+Created At: %s
+Published: %t
+Tags: %v
+----------------
+
+%s
+
+================
+
+
+`, b.Title, b.CreatedAt, b.Published, b.Tags, b.Content)
+}
+
+type Blogs struct {
+	collection *mongo.Collection
+}
+
+func NewBlogs(collection *mongo.Collection) *Blogs {
+	return &Blogs{
+		collection: collection,
+	}
+}
+
+func (b *Blogs) CreateSlug(title string) string {
+
+	base := slug.Make(title)
+	unique := base
+	counter := 1
+
+	for {
+		if !b.CheckBlogExistsBySlug(unique) {
+			break
+		}
+
+		counter++
+		unique = fmt.Sprintf("%s-%d", unique, counter)
+	}
+	return unique
+}
+
+// CREATE
+func (b *Blogs) AddBlog(blog BlogModel) error {
+	validate := validator.New()
+
+	if err := validate.Struct(blog); err != nil {
+		return errors.Join(errors.New("failed to validate blog"), err)
+	}
+
+	blog.ID = primitive.NewObjectID()
+	blog.CreatedAt = time.Now()
+	blog.Published = false
+	blog.Slug = b.CreateSlug(blog.Title)
+
+	if _, err := b.collection.InsertOne(context.TODO(), blog); err != nil {
+		return errors.Join(errors.New("failed to add blog to the database"), err)
+	}
+
+	return nil
+}
+
+// DELETE
+func (b *Blogs) DeleteBlogByID(id string) error {
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return errors.Join(errors.New("invalid object id provided"), err)
+	}
+	if _, err := b.collection.DeleteOne(context.TODO(), bson.M{
+		"_id": objectId,
+	}); err != nil {
+		return errors.Join(errors.New("failed to delete blog"), err)
+	}
+	return nil
+}
+
+// READ
+func (b *Blogs) FindBlogs(filter bson.M, pageNo, blogsPerPage int) ([]*BlogModel, error) {
+	limit := blogsPerPage
+	skip := (pageNo - 1) * limit
+
+	var blogs []*BlogModel
+	options := options.Find().SetSort(bson.M{"created_at": -1}).SetSkip(int64(skip)).SetLimit(int64(limit))
+
+	cursor, err := b.collection.Find(context.TODO(), filter, options)
+	if err != nil {
+		return nil, errors.Join(errors.New("failed to find blogs"), err)
+	}
+	defer cursor.Close(context.TODO())
+
+	if err := cursor.All(context.TODO(), &blogs); err != nil {
+		return nil, errors.Join(errors.New("failed to find blogs"), err)
+	}
+
+	return blogs, nil
+}
+
+func (b *Blogs) FindBlogByID(id string) (*BlogModel, error) {
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, errors.Join(errors.New("invalid object id provided"), err)
+	}
+
+	var blog BlogModel
+	if err := b.collection.FindOne(context.TODO(), bson.M{
+		"_id": objectId,
+	}).Decode(&blog); err != nil {
+		return nil, errors.Join(errors.New("failed to find blog by id"), err)
+	}
+	return &blog, nil
+}
+
+func (b *Blogs) FindBlogBySlug(slug string) (*BlogModel, error) {
+	var blog BlogModel
+	if err := b.collection.FindOne(context.TODO(), bson.M{
+		"slug": slug,
+	}).Decode(&blog); err != nil {
+		return nil, errors.Join(errors.New("failed to find blog by slug"), err)
+	}
+	return &blog, nil
+}
+
+func (b *Blogs) FindBlogsByTitle(title string, pageNo, blogsPerPage int) ([](*BlogModel), error) {
+	blogs, err := b.FindBlogs(bson.M{
+		"title": bson.M{
+			"$regex":   title,
+			"$options": "i",
+		},
+	}, pageNo, blogsPerPage)
+
+	return blogs, err
+}
+
+func (b *Blogs) CheckBlogExistsByID(id string) bool {
+	if found, err := b.FindBlogByID(id); found == nil || err != nil {
+		return false
+	}
+	return true
+}
+
+func (b *Blogs) CheckBlogExistsBySlug(slug string) bool {
+	if found, err := b.FindBlogBySlug(slug); found == nil || err != nil {
+		return false
+	}
+	return true
+}
+
+// UPDATE
+func (b *Blogs) PublishBlogByID(id string) error {
+	if !b.CheckBlogExistsByID(id) {
+		return errors.New("blog doesn't exists")
+	}
+
+	objectId, _ := primitive.ObjectIDFromHex(id)
+
+	_, err := b.collection.UpdateByID(context.TODO(), objectId, bson.M{
+		"$set": bson.M{
+			"published": true,
+		},
+	})
+	if err != nil {
+		return errors.Join(errors.New("failed to publish blog"), err)
+	}
+	return nil
+}
+
+func (b *Blogs) UpdateBlogByID(id string, body BlogModel) error {
+
+	validate := validator.New()
+
+	if err := validate.Struct(body); err != nil {
+		return errors.Join(errors.New("failed to validate blog"), err)
+	}
+
+	old, err := b.FindBlogByID(id)
+	if err != nil {
+		return errors.Join(errors.New("failed to update blog"), err)
+	}
+
+	updatedBlog := BlogModel{}
+	updatedBlog.ID = old.ID
+	updatedBlog.Published = old.Published
+	updatedBlog.CreatedAt = old.CreatedAt
+	updatedBlog.Slug = old.Slug
+
+	updatedBlog.Content = body.Content
+	updatedBlog.Tags = body.Tags
+	updatedBlog.Title = body.Title
+	if updatedBlog.Title != old.Title {
+		updatedBlog.Slug = b.CreateSlug(updatedBlog.Title)
+	}
+
+	if _, err := b.collection.UpdateByID(context.TODO(), updatedBlog.ID, bson.M{
+		"$set": updatedBlog,
+	}); err != nil {
+		return errors.Join(errors.New("failed to update blog"), err)
+	}
+
+	return nil
+}
