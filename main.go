7466ea61aefa19da7433fcff8a685810b40d5104
Muhammed Rajab <muhammedrajab101@gmail.com>
2023-11-21 20:18:07 +0530

    cleaned main.go from all unwanted dummy codes

--- main.go
+++ main.go
@@ -38,31 +38,4 @@ func main() {
 	routers.BlogRouter(root)
 
 	engine.Run(":8000")
-
-	// blogs := models.NewBlogs(db.GetMDB().BlogsCollection())
-	// post := models.BlogModel{
-	// 	Title: "Far, but close.",
-	// 	Desc:  "How can someone be so far away from you physically, yet so close to you spiritually?",
-	// 	Content: `<p style="text-align: center;">
-	// Amidst of all the chaos and fun,<br>
-	// I could only think of you.<br>
-	// It never fails to intrigue me,<br>
-	// How do you live inside my head rent free?
-	// <br>
-	// <br>
-	// We are at least a century miles away,<br>
-	// Yet I could smell you here,<br>
-	// feel your presence; feel your forehead,<br>
-	// getting warm as I leave a kiss mark on it.
-	// <br>
-	// <br>
-	// Four days just seem trivial, but are they?
-	// I guess not.<br>
-	// </p>`,
-	// 	Tags: []string{"love", "distance", "relationship"},
-	// }
-
-	// if err := blogs.AddBlog(post); err != nil {
-	// 	log.Fatal(err)
-	// }
 }
