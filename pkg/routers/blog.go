427c77315613bba33dcabe1e3b9fb87b705a8108
Muhammed Rajab <muhammedrajab101@gmail.com>
2023-11-21 20:21:54 +0530

    added few necessary comments to router

--- pkg/routers/blog.go
+++ pkg/routers/blog.go
@@ -15,28 +15,43 @@ func BlogRouter(root *gin.RouterGroup) *gin.RouterGroup {
 	{
 		dashboard := router.Group("/dashboard")
 		{
+			// Dashboard Home
 			dashboard.GET("", AuthMiddleware, controller.DashboardHandler)
 
+			// AUTH
 			dashboard.GET("/auth", controller.AuthDashboardHandler)
 			dashboard.POST("/auth", controller.AuthDashboard)
 			dashboard.GET("/auth/logout", controller.LogoutDashboard)
 
+			// BLOG CREATE
 			dashboard.GET("/add", AuthMiddleware, controller.AddBlogHandler)
 			dashboard.POST("/add", AuthMiddleware, controller.AddBlog)
 
+			// BLOG UPDATE
 			dashboard.GET("/edit/:id", AuthMiddleware, controller.EditBlogHandler)
 			dashboard.POST("/edit/:id", AuthMiddleware, controller.EditBlog)
 
+			// IMAGE READ
 			dashboard.GET("/images", AuthMiddleware, controller.ImagesHandler)
+
+			// IMAGE CREATE
 			dashboard.POST("/images", AuthMiddleware, controller.UploadImages)
+
+			// IMAGE DELETE
 			dashboard.POST("/images/:id", AuthMiddleware, controller.DeleteImage)
 
+			// BLOG DELETE
 			dashboard.DELETE("/:id", AuthMiddleware, controller.DeleteBlog)
+
+			// BLOG UPDATE
 			dashboard.PUT("/:id/toggle_publish", AuthMiddleware, controller.TogglePublishBlog)
 			dashboard.PUT("/:id", AuthMiddleware, controller.EditBlog)
 		}
 
+		// BLOG HOME
 		router.GET("", controller.HomeHandler)
+
+		// BLOG INDIVIDUAL
 		router.GET("/:slug", controller.BlogHandler)
 
 	}
