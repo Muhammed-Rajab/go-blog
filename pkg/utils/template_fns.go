24c087b893622536cf32fab913a4ecd99419ecff
Muhammed Rajab <muhammedrajab101@gmail.com>
2023-11-22 08:00:23 +0530

    added a few meta tags. will add other ones later. now gotta go to school.

--- pkg/utils/template_fns.go
+++ pkg/utils/template_fns.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"html/template"
+	"strings"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -9,6 +10,10 @@ import (
 
 func formatAsDate(t time.Time) string {
 	return t.Format("Monday, 2 May 2006")
+}
+
+func tagsAsString(tags []string) string {
+	return strings.Join(tags, ",")
 }
 
 func objectID(id primitive.ObjectID) string {
@@ -19,5 +24,6 @@ func GetTemplateFuncsMap() template.FuncMap {
 	return template.FuncMap{
 		"formatAsDate": formatAsDate,
 		"objectid":     objectID,
+		"tagsAsString": tagsAsString,
 	}
 }
